6f3bd7d98f659e030ffb737c7d146e022d3f90af
Jakub <5243212@gmail.com>
2021-01-05 15:56:51 +0300

    First commit Practice1

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat : false
+};
+
+const a = prompt('Один из последний просмотренных фильмов?', ""),
+      b = prompt ('На сколько оценице его?', ""),
+      c = prompt ('Один из последний просмотренных фильмов?', ""),
+      d = prompt ('На сколько оценице его?', "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
